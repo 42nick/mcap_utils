b8409cbb76a5ec6e71c7cbf42315804664db80d1
42nick <niklashahn98@googlemail.com>
2023-10-07 09:42:45 +0200

    first commit

---
+++ src/mcap_utils/main.py
@@ -0,0 +1,15 @@
+def main() -> None:
+    """
+    The core function of this awesome project.
+    """
+    
+    print(f"Hello world :)")
+    
+    # open and write an mcap file
+    with open("test.mcap", "w") as f:
+        # write a 3d box with world coordinates to the file
+        
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from mcap_utils.main import main
+
+
+def test_main() -> None:
+    main()
